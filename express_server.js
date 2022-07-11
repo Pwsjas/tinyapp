5ebd41ef7796fbb157f13f30803f479d021606bc
Philip Simpson <pwsjas433@hotmail.com>
2022-07-11 18:50:51 +0000

    added a partials folder with _header, referenced it within the body of urls_show and urls_index

--- express_server.js
+++ express_server.js
@@ -18,10 +18,19 @@ app.get("/hello", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+//new
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+//new
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
