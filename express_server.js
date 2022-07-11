f0ecd035614cbfb934f6e53770125b4943ef5871
Philip Simpson <pwsjas433@hotmail.com>
2022-07-11 19:29:34 +0000

    added a urls_new file, containing a form that makes POST requests to /url, also breifly created a 6 letter random character generator

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,13 +21,26 @@ app.get("/urls.json", (req, res) => {
 });
 
 //new
+//View All tinyURLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+//Create New tinyURL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+//View Specific tinyURL
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 //new
 
@@ -34,3 +48,19 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let generatedString = '';
+  let random = 0;
+
+  for (let i = 0; i < 6; i++) {
+    random = Math.ceil(Math.random() * 36);
+
+    if (random <= 10) {
+      generatedString += random;
+    } else {
+      generatedString += String.fromCharCode(random + 76);
+    }
+  }
+  return generatedString;
+}
