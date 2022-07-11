10e40a5ab14434776b7fc0cebe95b770338d4da0
Philip Simpson <pwsjas433@hotmail.com>
2022-07-11 18:37:16 +0000

    added urls_index.ejs and provided content to /urls

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
